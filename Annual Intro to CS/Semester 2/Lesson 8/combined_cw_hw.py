6cae0ee495748ecae246fa66e5a537414188af00
dfn-slxxp <waldman.sebastian@gmail.com>
2026-02-05 17:25:10 +0000

    Complete Lesson 8

---
+++ Annual Intro to CS/Semester 2/Lesson 8/combined_cw_hw.py
@@ -0,0 +1,28 @@
+# Classwork
+
+def sumFromZeroToN(n):
+    if n <= 0:
+        return 0
+    else:
+        counter = 0
+        while True:
+            if(n == 0):
+                break
+            else:
+                counter += n
+                n -= 1
+        return counter
+
+
+
+# Homework
+
+def sumDigits(n):
+    count = 0
+    while True:
+        if n == 0:
+            break
+        else:
+            count += n % 10
+            n = n // 10
+    return count
